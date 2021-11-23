48eb6e09f06bf4bcd77943e0aacd409ecbc4b1f3
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-11-24 00:13:44 +0700

    try a different code of CFG2CNF, stored in CFG2CNF.py

--- CFG2CNF.py
+++ CFG2CNF.py
@@ -1,147 +1,84 @@
-# -*- coding: utf-8 -*-
-#IT's assumed that starting variable is the first typed
-import sys, helper
-
-left, right = 0, 1
-
-K, V, Productions = [],[],[]
-variablesJar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z"
-"A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1", "J1", "K1", "L1", "M1", "N1", "O1", "P1", "Q1", "R1", "S1", "T1", "U1", "W1", "X1", "Y1", "Z1",
-"A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2", "I2", "J2", "K2", "L2", "M2", "N2", "O2", "P2", "Q2", "R2", "S2", "T2", "U2", "W2", "X2", "Y2", "Z2",
-"A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3", "I3", "J3", "K3", "L3", "M3", "N3", "O3", "P3", "Q3", "R3", "S3", "T3", "U3", "W3", "X3", "Y3", "Z3"]
+# Global dictionary used for storing the rules.
+RULE_DICT = {}
 
 
-def isUnitary(rule, variables):
-	if rule[left] in variables and rule[right][0] in variables and len(rule[right]) == 1:
-		return True
-	return False
-
-def isSimple(rule):
-	if rule[left] in V and rule[right][0] in K and len(rule[right]) == 1:
-		return True
-	return False
-
-
-for nonTerminal in V:
-	if nonTerminal in variablesJar:
-		variablesJar.remove(nonTerminal)
-
-#Add S0->S rule––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––START
-def START(productions, variables):
-	variables.append('S0')
-	return [('S0', [variables[0]])] + productions
-#Remove rules containing both terms and variables, like A->Bc, replacing by A->BZ and Z->c–––––––––––TERM
-def TERM(productions, variables):
-	newProductions = []
-	#create a dictionari for all base production, like A->a, in the form dic['a'] = 'A'
-	dictionary = helper.setupDict(productions, variables, terms=K)
-	for production in productions:
-		#check if the production is simple
-		if isSimple(production):
-			#in that case there is nothing to change
-			newProductions.append(production)
-		else:
-			for term in K:
-				for index, value in enumerate(production[right]):
-					if term == value and not term in dictionary:
-						#it's created a new production vaiable->term and added to it 
-						dictionary[term] = variablesJar.pop()
-						#Variables set it's updated adding new variable
-						V.append(dictionary[term])
-						newProductions.append( (dictionary[term], [term]) )
-						
-						production[right][index] = dictionary[term]
-					elif term == value:
-						production[right][index] = dictionary[term]
-			newProductions.append( (production[left], production[right]) )
-			
-	#merge created set and the introduced rules
-	return newProductions
-
-#Eliminate non unitry rules––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––BIN
-def BIN(productions, variables):
-	result = []
-	for production in productions:
-		k = len(production[right])
-		#newVar = production[left]
-		if k <= 2:
-			result.append( production )
-		else:
-			newVar = variablesJar.pop(0)
-			variables.append(newVar+'1')
-			result.append( (production[left], [production[right][0]]+[newVar+'1']) )
-			i = 1
-#TODO
-			for i in range(1, k-2 ):
-				var, var2 = newVar+str(i), newVar+str(i+1)
-				variables.append(var2)
-				result.append( (var, [production[right][i], var2]) )
-			result.append( (newVar+str(k-2), production[right][k-2:k]) ) 
-	return result
-	
-
-#Delete non terminal rules–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––DEL
-def DEL(productions):
-	newSet = []
-	#seekAndDestroy throw back in:
-	#        – outlaws all left side of productions such that right side is equal to the outlaw
-	#        – productions the productions without outlaws 
-	outlaws, productions = helper.seekAndDestroy(target='e', productions=productions)
-	#add new reformulation of old rules
-	for outlaw in outlaws:
-		#consider every production: old + new resulting important when more than one outlaws are in the same prod.
-		for production in productions + [e for e in newSet if e not in productions]:
-			#if outlaw is present in the right side of a rule
-			if outlaw in production[right]:
-				#the rule is rewrited in all combination of it, rewriting "e" rather than outlaw
-				#this cycle prevent to insert duplicate rules
-				newSet = newSet + [e for e in  helper.rewrite(outlaw, production) if e not in newSet]
-
-	#add unchanged rules and return
-	return newSet + ([productions[i] for i in range(len(productions)) 
-							if productions[i] not in newSet])
-
-def unit_routine(rules, variables):
-	unitaries, result = [], []
-	#controllo se una regola è unaria
-	for aRule in rules:
-		if isUnitary(aRule, variables):
-			unitaries.append( (aRule[left], aRule[right][0]) )
-		else:
-			result.append(aRule)
-	#altrimenti controllo se posso sostituirla in tutte le altre
-	for uni in unitaries:
-		for rule in rules:
-			if uni[right]==rule[left] and uni[left]!=rule[left]:
-				result.append( (uni[left],rule[right]) )
-	
-	return result
-
-def UNIT(productions, variables):
-	i = 0
-	result = unit_routine(productions, variables)
-	tmp = unit_routine(result, variables)
-	while result != tmp and i < 1000:
-		result = unit_routine(tmp, variables)
-		tmp = unit_routine(result, variables)
-		i+=1
-	return result
+def read_grammar(grammar_file):
+    """
+    Reads in the given grammar file and splits it into separate lists for each rule.
+    :param grammar_file: the grammar file to read in.
+    :return: the list of rules.
+    """
+    with open(grammar_file) as cfg:
+        lines = cfg.readlines()
+    return [x.replace("->", "").split() for x in lines]
 
 
-if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		modelPath = str(sys.argv[1])
-	else:
-		modelPath = 'model.txt'
-	
-	K, V, Productions = helper.loadModel( modelPath )
+def add_rule(rule):
+    """
+    Adds a rule to the dictionary of lists of rules.
+    :param rule: the rule to add to the dict.
+    """
+    global RULE_DICT
 
-	Productions = START(Productions, variables=V)
-	Productions = TERM(Productions, variables=V)
-	Productions = BIN(Productions, variables=V)
-	Productions = DEL(Productions)
-	Productions = UNIT(Productions, variables=V)
-	
-	print( helper.prettyForm(Productions) )
-	print( len(Productions) )
-	open('out.txt', 'w').write(helper.prettyForm(Productions) )
+    if rule[0] not in RULE_DICT:
+        RULE_DICT[rule[0]] = []
+    RULE_DICT[rule[0]].append(rule[1:])
+
+
+def convert_grammar(grammar):
+    """
+        Converts a context-free grammar in the form of
+        S -> NP VP
+        NP -> Det ADV N
+        and so on into a chomsky normal form of that grammar. After the conversion rules have either
+        exactly one terminal symbol or exactly two non terminal symbols on its right hand side.
+        Therefore some new non terminal symbols might be created. These non terminal symbols are
+        named like the symbol they replaced with an appended index.
+    :param grammar: the CFG.
+    :return: the given grammar converted into CNF.
+    """
+
+    # Remove all the productions of the type A -> X B C or A -> B a.
+    global RULE_DICT
+    unit_productions, result = [], []
+    res_append = result.append
+    index = 0
+
+    for rule in grammar:
+        new_rules = []
+        if len(rule) == 2 and rule[1][0] != "'":
+            # Rule is in form A -> X, so back it up for later and continue with the next rule.
+            unit_productions.append(rule)
+            add_rule(rule)
+            continue
+        elif len(rule) > 2:
+            # Rule is in form A -> X B C [...] or A -> X a.
+            terminals = [(item, i) for i, item in enumerate(rule) if item[0] == "'"]
+            if terminals:
+                for item in terminals:
+                    # Create a new non terminal symbol and replace the terminal symbol with it.
+                    # The non terminal symbol derives the replaced terminal symbol.
+                    rule[item[1]] = f"{rule[0]}{str(index)}"
+                    new_rules += [f"{rule[0]}{str(index)}", item[0]]
+                index += 1
+            while len(rule) > 3:
+                new_rules.append([f"{rule[0]}{str(index)}", rule[1], rule[2]])
+                rule = [rule[0]] + [f"{rule[0]}{str(index)}"] + rule[3:]
+                index += 1
+        # Adds the modified or unmodified (in case of A -> x i.e.) rules.
+        add_rule(rule)
+        res_append(rule)
+        if new_rules:
+            result.extend(new_rules)
+    # Handle the unit productions (A -> X)
+    while unit_productions:
+        rule = unit_productions.pop()
+        if rule[1] in RULE_DICT:
+            for item in RULE_DICT[rule[1]]:
+                new_rule = [rule[0]] + item
+                if len(new_rule) > 2 or new_rule[1][0] == "'":
+                    result.insert(0, new_rule)
+                else:
+                    unit_productions.append(new_rule)
+                add_rule(new_rule)
+    return result
