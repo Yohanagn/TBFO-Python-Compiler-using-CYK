789c19b3846d2f07182a56aca02293b73a48c6e9
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-11-24 15:07:31 +0700

    add new tc

---
+++ tescase5.py
@@ -0,0 +1,11 @@
+# input S
+S = 10
+
+# berdasarkan gambar panjang CP adalah S/2
+# berdasarkan gambar R sin 30 = r, maka dari itu R = 2r, dan hasil dari R-r = r
+# sehingga yang kita cari adalah r
+# r = 1/3 tinggi segitiga
+# tinggi segitiga = S/2 akar 3
+
+# maka...
+print(1/3 * (S / 2 * (3 ** (1/2))))
